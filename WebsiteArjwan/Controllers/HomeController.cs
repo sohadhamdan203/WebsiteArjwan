080029b3ce3def806336acba381c1135bea5db95
DESKTOP-UECHB2U\HP <hajjajsohad@gmail.com>
2021-01-25 09:43:49 +0200

    Add project files.

---
+++ WebsiteArjwan/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteArjwan.Models;
+
+namespace WebsiteArjwan.Controllers
+{
+    public class HomeController : Controller
+    {
+        ArjwanTechEntities db = new ArjwanTechEntities();
+        public ActionResult Index()
+        {
+           List<Service> list = db.Services.ToList();
+           return View(list);
+        }
+
+        public ActionResult About()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            List<Service> list = db.Services.ToList();
+            return View(list);
+        }
+
+        public ActionResult Portfoilo()
+        {
+            List<Portfoilo> lista = db.Portfoiloes.ToList();
+            return View(lista);
+        }
+    }
+}
